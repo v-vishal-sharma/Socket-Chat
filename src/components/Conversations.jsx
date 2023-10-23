14064eec9a745880032b7e5ab7c7ad392ca92d10
Vishal Sharma <vishals200415@gmail.com>
2023-10-23 23:08:01 +0530

    :)

--- src/components/Conversations.jsx
+++ src/components/Conversations.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { ListGroup } from 'react-bootstrap'
-import { useConversations } from '../contexts/ConversationsProvider'
+import React from "react";
+import { ListGroup } from "react-bootstrap";
+import { useConversations } from "../contexts/ConversationsProvider";
 
 export default function Conversations() {
-
-  const { conversations, selectConversationIndex } = useConversations()
+  const { conversations, selectConversationIndex } = useConversations();
 
   return (
     // variant attribute will get rid of the side borders since we already have borders
-    <ListGroup variant='flush'>
+    <ListGroup variant="flush">
       {conversations.map((conversation, index) => (
         <ListGroup.Item
           key={index}
@@ -16,9 +15,9 @@ export default function Conversations() {
           active={conversation.selected}
           onClick={() => selectConversationIndex(index)}
         >
-          {conversation.recipients.map(r => r.name).join(', ')}
+          {conversation.recipients.map((r) => r.name).join(", ")}
         </ListGroup.Item>
       ))}
     </ListGroup>
-  )
+  );
 }
